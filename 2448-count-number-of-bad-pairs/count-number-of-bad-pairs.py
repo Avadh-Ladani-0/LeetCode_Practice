f0123ff59775bf223a6994c073d585b4cb66ab24
Avadh Ladani <avadhladani2002@gmail.com>
2025-02-09 08:31:56 +0530

    Time: 77 ms (62.14%) | Memory: 38.9 MB (28.81%) - LeetSync

---
+++ 2448-count-number-of-bad-pairs/count-number-of-bad-pairs.py
@@ -0,0 +1,27 @@
+class Solution:
+    def countBadPairs(self, nums: List[int]) -> int:
+        # Brute force
+        # count=0
+        # for i in range(len(nums)-1):
+        #     for j in range(i+1,len(nums)):
+        #         print(((j-i),(nums[j]-nums[i])))
+        #         if j-i!=nums[j]-nums[i]:
+        #             count+=1
+        # return count
+
+        bad_pairs = 0
+        diff_count = {}
+
+        for pos in range(len(nums)):
+            diff = pos - nums[pos]
+
+            # Count of previous positions with same difference
+            good_pairs_count = diff_count.get(diff, 0)
+
+            # Total possible pairs minus good pairs = bad pairs
+            bad_pairs += pos - good_pairs_count
+
+            # Update count of positions with this difference
+            diff_count[diff] = good_pairs_count + 1
+
+        return bad_pairs
