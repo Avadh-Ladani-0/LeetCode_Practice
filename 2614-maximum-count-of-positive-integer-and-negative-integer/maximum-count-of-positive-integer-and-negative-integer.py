818f5f9303da4a6212f5e4ef9da53b38cb5cb4e1
Avadh Ladani <avadhladani2002@gmail.com>
2025-03-12 08:00:31 +0530

    Time: 1 ms (40.59%) | Memory: 18 MB (41.77%) - LeetSync

---
+++ 2614-maximum-count-of-positive-integer-and-negative-integer/maximum-count-of-positive-integer-and-negative-integer.py
@@ -0,0 +1,12 @@
+class Solution:
+    def maximumCount(self, nums: List[int]) -> int:
+        neg_count=0
+        pos_count=0
+
+        for i in nums:
+            if i<0:
+                neg_count+=1
+            elif i>0:
+                pos_count+=1
+        
+        return max(neg_count,pos_count)
